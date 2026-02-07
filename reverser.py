8dda65bc31172cae003ff0536e39daf0db8184bd
Lucifer <smp.pourrr@gmail.com>
2026-02-07 11:13:02 +0330

    adding a reverser for the texts

---
+++ reverser.py
@@ -0,0 +1,9 @@
+
+with open("original.txt", "r") as file:
+    file.write("original = input(\"Enter your text: \")")
+
+'''
+original = input("Enter your text: ")
+reversed_text = original[::-1]
+print("Reversed text:", reversed_text)
+'''
